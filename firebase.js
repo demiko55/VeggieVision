b2e367194c9656f1c048903a42315f43c6055e29
Demi <cbgaojunmeng@gmail.com>
2023-07-24 18:37:13 -0700

    add env config

--- firebase.js
+++ firebase.js
@@ -1,18 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import {getAuth, GoogleAuthProvider} from 'firebase/auth';
+import {APP_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID} from '@env'
+
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAodrAvOheiuxAYUnjR58SThbkI5mGRfzE",
-  authDomain: "veggieversion-630eb.firebaseapp.com",
-  projectId: "veggieversion-630eb",
-  storageBucket: "veggieversion-630eb.appspot.com",
-  messagingSenderId: "879205778433",
-  appId: "1:879205778433:web:594639cf8f8eb5f8ceec00"
+  apiKey: APP_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID
 };
 
 // Initialize Firebase
